48fbfd75240637743fd3e256259165b9af38310a
frank015940 <muchirifranklin8@gmail.com>
2022-11-30 00:27:44 -0800

    Task 0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,39 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * @filename: name of text file to be read
+ * @letters: number of letters to be read from the file
+ *
+ * Return: the actual number of letters it could read and print \n
+ *if the file can not be opened or read, or is NULL, return 0
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	ssize_t readfnrv;
+	int fdnum;
 	char *buffer;
 
-	if (filename == NULL)
-		return (0);
+	fdnum = open(filename, O_RDONLY);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	if (fdnum == -1)
 	{
-		free(buffer);
 		return (0);
 	}
+	else
+	{
+		buffer = malloc(letters);
 
-	free(buffer);
-	close(o);
+		readfnrv = read(fdnum, buffer, letters);
 
-	return (w);
+		write(STDOUT_FILENO, buffer, readfnrv);
+
+		close(fdnum);
+
+		free(buffer);
+	}
+
+	return (readfnrv);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,35 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * create_file - creates a file
  *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * @filename: name of file to be created
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd;
 
-	if (filename == NULL)
-		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if (text_content != NULL)
+	if (fd == -1)
 	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
 		return (-1);
+	}
+	else
+	{
+		if (text_content != NULL)
+		{
+			int strln = strlen(text_content);
 
-	close(o);
+			write(fd, text_content, strln);
+		}
+
+		close(fd);
+	}
 
 	return (1);
 }
